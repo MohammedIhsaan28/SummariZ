0fda7f1cc5ee1465067c7fbd8f098f4e20ec041c
Mohammed Ihsaan <mohammedihsaan28@gmail.com>
2025-12-04 19:48:03 +0530

    chore: update allowedDevOrigins in next.config.ts to use port 3001 and fix package name in package.json and package-lock.json

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     allowedDevOrigins: [
-      "http://localhost:3000",
+      "http://localhost:3001",
       "https://*.ngrok-free.app",
     ],
   } as any,
